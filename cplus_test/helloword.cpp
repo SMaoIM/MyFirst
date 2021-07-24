a99690870d5e2271e7f7366d9aa4639553b19950
SMaoIM <maoshuzhou1@hotmail.com>
2021-07-24 22:56:43 +0800

    my first formal git reposity

---
+++ cplus_test/helloword.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   
+    printf("this is a test\n");
+    cout<<"hello,world"<<endl;
+}
---
+++ helloworld/my_ctest.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"hello world"<<endl;
+    printf("this is my first test\n");
+}
